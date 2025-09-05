5b3a5268034eed99f090b9b159fafcf97f1ef64a
KelvinTang <tangke3@oocl.com>
2025-09-05 23:10:05 +0800

    test: add move east test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void should_move_when_executeCommand_given_M() {
+    void should_move_when_executeCommand_given_M_and_direction_is_N() {
         //Given
         Direction direction = new Direction('N');
         Location location = new Location(0, 0, direction);
@@ -16,6 +16,21 @@ class DemoTest {
 
         //Then
         Location expectedLocation = new Location(0, 1, direction);
+        assertEquals(expectedLocation.toString(), marsRover.getLocation().toString());
+    }
+
+    @Test
+    void should_move_when_executeCommand_given_M_and_direction_is_E() {
+        //Given
+        Direction direction = new Direction('E');
+        Location location = new Location(0, 0, direction);
+        MarsRover marsRover = new MarsRover(location);
+
+        //When
+        marsRover.executeCommand('M');
+
+        //Then
+        Location expectedLocation = new Location(1, 0, direction);
         assertEquals(expectedLocation.toString(), marsRover.getLocation().toString());
     }
 }
