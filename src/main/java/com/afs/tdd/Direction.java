26e7d88f2720e737c25e735578b2175ab0b8d801
KelvinTang <tangke3@oocl.com>
2025-09-05 16:25:02 +0800

    feat: add turnLeft, turnRight

--- src/main/java/com/afs/tdd/Direction.java
+++ src/main/java/com/afs/tdd/Direction.java
@@ -1,7 +1,11 @@
 package com.afs.tdd;
 
+import java.util.List;
+
 public class Direction {
+
     private char direction;
+    List<Character> directionList = List.of('N', 'E', 'S', 'W');
 
     public char getDirection() {
         return direction;
@@ -11,11 +15,19 @@ public class Direction {
         this.direction = direction;
     }
 
+    public int getDirectionIndex(char direction) {
+        return directionList.indexOf(direction);
+    }
+
     public Direction(char direction) {
         this.direction = direction;
     }
 
     public void turnLeft() {
+        setDirection(directionList.get(Math.abs(getDirectionIndex(direction)+1)%directionList.size()));
+    }
 
+    public void turnRight() {
+        setDirection(directionList.get(Math.abs(getDirectionIndex(direction)-1)%directionList.size()));
     }
 }
