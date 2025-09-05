5da4e1aee9bf98d25e008d9bc0f4f3586443fc33
KelvinTang <tangke3@oocl.com>
2025-09-05 23:13:22 +0800

    test: add move west test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -48,4 +48,19 @@ class DemoTest {
         Location expectedLocation = new Location(0, -1, direction);
         assertEquals(expectedLocation.toString(), marsRover.getLocation().toString());
     }
+
+    @Test
+    void should_move_when_executeCommand_given_M_and_direction_is_W() {
+        //Given
+        Direction direction = new Direction('W');
+        Location location = new Location(0, 0, direction);
+        MarsRover marsRover = new MarsRover(location);
+
+        //When
+        marsRover.executeCommand('M');
+
+        //Then
+        Location expectedLocation = new Location(-1, 0, direction);
+        assertEquals(expectedLocation.toString(), marsRover.getLocation().toString());
+    }
 }
