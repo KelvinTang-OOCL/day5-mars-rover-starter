d8254c2cfb1ebf611e1db763710b65c1dabc7715
KelvinTang <tangke3@oocl.com>
2025-09-05 23:25:37 +0800

    test: add turn right with direction is E test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void should_move_when_executeCommand_given_M_and_direction_is_N() {
+    void should_increase_coordinateY_when_executeCommand_given_M_and_direction_is_N() {
         //Given
         Direction direction = new Direction('N');
         Location location = new Location(0, 0, direction);
@@ -20,7 +20,7 @@ class DemoTest {
     }
 
     @Test
-    void should_move_when_executeCommand_given_M_and_direction_is_E() {
+    void should_increase_coordinateX_when_executeCommand_given_M_and_direction_is_E() {
         //Given
         Direction direction = new Direction('E');
         Location location = new Location(0, 0, direction);
@@ -35,7 +35,7 @@ class DemoTest {
     }
 
     @Test
-    void should_move_when_executeCommand_given_M_and_direction_is_S() {
+    void should_decrease_coordinateY_when_executeCommand_given_M_and_direction_is_S() {
         //Given
         Direction direction = new Direction('S');
         Location location = new Location(0, 0, direction);
@@ -50,7 +50,7 @@ class DemoTest {
     }
 
     @Test
-    void should_move_when_executeCommand_given_M_and_direction_is_W() {
+    void should_decrease_coordinateX_when_executeCommand_given_M_and_direction_is_W() {
         //Given
         Direction direction = new Direction('W');
         Location location = new Location(0, 0, direction);
@@ -65,7 +65,7 @@ class DemoTest {
     }
 
     @Test
-    void should_move_when_executeCommand_given_L_and_direction_is_N() {
+    void should_update_direction_to_W_when_executeCommand_given_L_and_direction_is_N() {
         //Given
         Direction direction = new Direction('N');
         Location location = new Location(0, 0, direction);
@@ -80,7 +80,7 @@ class DemoTest {
     }
 
     @Test
-    void should_move_when_executeCommand_given_L_and_direction_is_W() {
+    void should_update_direction_to_S_when_executeCommand_given_L_and_direction_is_W() {
         //Given
         Direction direction = new Direction('W');
         Location location = new Location(0, 0, direction);
@@ -95,7 +95,7 @@ class DemoTest {
     }
 
     @Test
-    void should_move_when_executeCommand_given_L_and_direction_is_S() {
+    void should_update_direction_to_E_when_executeCommand_given_L_and_direction_is_S() {
         //Given
         Direction direction = new Direction('S');
         Location location = new Location(0, 0, direction);
@@ -110,7 +110,7 @@ class DemoTest {
     }
 
     @Test
-    void should_move_when_executeCommand_given_L_and_direction_is_E() {
+    void should_update_direction_to_N_when_executeCommand_given_L_and_direction_is_E() {
         //Given
         Direction direction = new Direction('E');
         Location location = new Location(0, 0, direction);
@@ -125,7 +125,7 @@ class DemoTest {
     }
 
     @Test
-    void should_move_when_executeCommand_given_R_and_direction_is_N() {
+    void should_update_direction_to_E_when_executeCommand_given_R_and_direction_is_N() {
         //Given
         Direction direction = new Direction('N');
         Location location = new Location(0, 0, direction);
@@ -136,6 +136,21 @@ class DemoTest {
 
         //Then
         Location expectedLocation = new Location(0, 0, new Direction('E'));
+        assertEquals(expectedLocation.toString(), marsRover.getLocation().toString());
+    }
+
+    @Test
+    void should_update_direction_to_S_when_executeCommand_given_R_and_direction_is_E() {
+        //Given
+        Direction direction = new Direction('E');
+        Location location = new Location(0, 0, direction);
+        MarsRover marsRover = new MarsRover(location);
+
+        //When
+        marsRover.executeCommand('R');
+
+        //Then
+        Location expectedLocation = new Location(0, 0, new Direction('S'));
         assertEquals(expectedLocation.toString(), marsRover.getLocation().toString());
     }
 }
