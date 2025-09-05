7f7f7be7e2e7d0dfacae3747a638dfeba30e59fb
KelvinTang <tangke3@oocl.com>
2025-09-05 23:16:01 +0800

    test: add turn left with direction is N test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -63,4 +63,19 @@ class DemoTest {
         Location expectedLocation = new Location(-1, 0, direction);
         assertEquals(expectedLocation.toString(), marsRover.getLocation().toString());
     }
+
+    @Test
+    void should_move_when_executeCommand_given_L_and_direction_is_N() {
+        //Given
+        Direction direction = new Direction('N');
+        Location location = new Location(0, 0, direction);
+        MarsRover marsRover = new MarsRover(location);
+
+        //When
+        marsRover.executeCommand('L');
+
+        //Then
+        Location expectedLocation = new Location(0, 0, new Direction('W'));
+        assertEquals(expectedLocation.toString(), marsRover.getLocation().toString());
+    }
 }
