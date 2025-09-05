eb6d5cb0ca1ebea2840e5cf82169fd60b502e86f
KelvinTang <tangke3@oocl.com>
2025-09-05 16:44:08 +0800

    feat: add turnLeft, turnRight, moveForward, moveBackward

--- src/main/java/com/afs/tdd/Location.java
+++ src/main/java/com/afs/tdd/Location.java
@@ -1,19 +1,30 @@
 package com.afs.tdd;
 
+import java.util.List;
+import java.util.Map;
+
 public class Location {
     private int coordinateX;
     private int coordinateY;
     private Direction direction;
+    private final int moveDistance = 1;
+    private final Map<Character, List<Integer>> moveDirectionInCoordinate =
+            Map.of('N', List.of(0,1),
+                    'E', List.of(1,0),
+                    'W', List.of(0,-1),
+                    'S', List.of(-1,0));
 
     public Location(int coordinateX, int coordinateY, Direction direction) {
-
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+        this.direction = direction;
     }
 
     public int getCoordinateX() {
         return coordinateX;
     }
 
-    public void setCoordinateX(int coordinateX) {
+    private void setCoordinateX(int coordinateX) {
         this.coordinateX = coordinateX;
     }
 
@@ -21,7 +32,7 @@ public class Location {
         return coordinateY;
     }
 
-    public void setCoordinateY(int coordinateY) {
+    private void setCoordinateY(int coordinateY) {
         this.coordinateY = coordinateY;
     }
 
@@ -29,7 +40,25 @@ public class Location {
         return direction;
     }
 
-    public void setDirection(Direction direction) {
+    private void setDirection(Direction direction) {
         this.direction = direction;
+    }
+
+    public void turnLeft() {
+        direction.turnLeft();
+    }
+
+    public void turnRight() {
+        direction.turnRight();
+    }
+
+    public void moveForward() {
+        setCoordinateX(coordinateX+moveDirectionInCoordinate.get(direction.getDirection()).get(0));
+        setCoordinateY(coordinateY+moveDirectionInCoordinate.get(direction.getDirection()).get(1));
+    }
+
+    public void moveBackward() {
+        setCoordinateX(coordinateX-moveDirectionInCoordinate.get(direction.getDirection()).get(0));
+        setCoordinateY(coordinateY-moveDirectionInCoordinate.get(direction.getDirection()).get(1));
     }
 }
