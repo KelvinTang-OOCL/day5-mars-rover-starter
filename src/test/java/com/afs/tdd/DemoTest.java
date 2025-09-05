ccd8a2eeb12b6ec7bcc290e10ec9edbf1b0dd7e7
KelvinTang <tangke3@oocl.com>
2025-09-05 23:19:51 +0800

    test: add turn left with direction is S test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -93,4 +93,19 @@ class DemoTest {
         Location expectedLocation = new Location(0, 0, new Direction('S'));
         assertEquals(expectedLocation.toString(), marsRover.getLocation().toString());
     }
+
+    @Test
+    void should_move_when_executeCommand_given_L_and_direction_is_S() {
+        //Given
+        Direction direction = new Direction('S');
+        Location location = new Location(0, 0, direction);
+        MarsRover marsRover = new MarsRover(location);
+
+        //When
+        marsRover.executeCommand('L');
+
+        //Then
+        Location expectedLocation = new Location(0, 0, new Direction('E'));
+        assertEquals(expectedLocation.toString(), marsRover.getLocation().toString());
+    }
 }
